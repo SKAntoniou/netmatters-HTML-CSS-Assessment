cdc3b3c19d67de43e348104e04a78ccb09772b67
SKAntoniou <SKAntoniou@gmail.com>
2025-03-21 14:40:28 +0000

    sidenav update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,11 +3,12 @@
   const headerHamburger = document.getElementById('header-hamburger');
   const sidenavContainer = document.getElementsByClassName('sidenav')[0];
   const bodyContent = document.getElementsByClassName('body-content')[0];
+  const bodyContentCover = document.getElementsByClassName('body-content-cover')[0];
 
   let expanded = false;
   function expandSideNav() {
-    sidenavContainer.classList.toggle('sidenav-expand');
     bodyContent.classList.toggle('body-content-move');
+    bodyContentCover.classList.toggle('body-content-cover-move');
     expanded = !expanded;
   }
 
@@ -15,7 +16,7 @@
     expandSideNav();
     event.stopPropagation(); // Stop body triggering when hamburger is triggered.
   });
-  bodyContent.addEventListener('click', () => {
+  bodyContentCover.addEventListener('click', () => {
     if (expanded) { expandSideNav(); }
   });
 }
